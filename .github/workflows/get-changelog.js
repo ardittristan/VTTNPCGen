c954464934452acdec35350cbf7c5832a0861c9b
ardittristan <ardittristan@gmail.com>
2021-03-19 18:58:24 +0100

    update workflow

--- .github/workflows/get-changelog.js
+++
@@ -1,3 +0,0 @@
-var fs = require("fs");
-const rootFiles = fs.readdirSync("/");
-console.log(JSON.parse(fs.readFileSync("module.json", "utf8")).url + "/blob/master/" + rootFiles.filter((file) => file.toLowerCase().includes("changelog"))?.[0]);
